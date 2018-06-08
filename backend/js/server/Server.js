e18d59393edb826fcd77a2eb474d59067358fa7c
Roland Stauffer <rstauffe@hsr.ch>
2018-06-08 10:54:16 +0200

    change

---
+++ backend/js/server/Server.js
@@ -0,0 +1,28 @@
+const http = require('http');
+const express = require('express');
+
+class Server {
+    constructor() {
+        this.HOSTNAME = "localhost";
+        this.PORT = 3000;
+        this.ROUTE_NOTE = "/note";
+        this.myServer        = express();
+        this.routerNote = express.Router();
+        this.initRouter();
+        this.start();
+    }
+
+    initRouter() {
+        this.routerNote.get('/', (req, res) => {
+            res.json({ message: 'hooray! welcome to our api!' });
+        });
+    }
+
+    start() {
+        this.myServer.use(this.ROUTE_NOTE, this.routerNote);
+        this.myServer.listen(this.PORT);
+        console.log("Server is running: http://" + this.HOSTNAME + ":" + this.PORT + this.ROUTE_NOTE);
+    }
+}
+
+new Server();
